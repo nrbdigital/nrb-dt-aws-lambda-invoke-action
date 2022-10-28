ec088a481d72882db3a84947eebe5de4dd038860
Bruno Franki <bruno.franki@nrb.be>
2022-10-28 14:35:04 +0200

    chore: add logic

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {InvokeCommand, LambdaClient, LogType} from '@aws-sdk/client-lambda'
+import fs from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const functionName = core.getInput('function-name', {required: true})
+    const payloadFile = core.getInput('payload-file')
+    // const functionName = 'spring-batch-management'
+    // const payloadFile = '/home/onurb/workspace/tmp/generator/ethias-oi-testgenerator2-batch/src/config/batch-dev.json'
+    const payload = core.getInput('payload')
+    let region = core.getInput('region')
+    if (region === '') region = 'eu-west-1'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let requestPayload: Uint8Array
+    if (payloadFile !== '') {
+      requestPayload = fs.readFileSync(payloadFile)
+    } else {
+      requestPayload = Buffer.from(payload)
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    // const requestPayload = fs.readFileSync(payloadFile)
+
+    const client = new LambdaClient({region})
+
+    const command = new InvokeCommand({
+      FunctionName: functionName,
+      Payload: requestPayload,
+      LogType: LogType.Tail,
+    })
+
+    const {Payload, LogResult} = await client.send(command)
+    let resultPayload: string
+    let logs: string
+    if (LogResult) {
+      logs = Buffer.from(LogResult, 'base64').toString()
+      console.log(logs)
+    }
+    if (Payload) {
+      resultPayload = Buffer.from(Payload).toString()
+      console.log('Result: ', resultPayload)
+      const result: any = JSON.parse(resultPayload)
+      if (result.errorType) {
+        throw new Error(`Lambda invocation failed: ${result.errorMessage}`)
+      }
+    }
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
